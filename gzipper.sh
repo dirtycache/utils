562c5e7117637bd72298833a9378e22913b26828
Adam Korab <adam.korab@sixpackets.com>
2020-09-24 20:35:30 -0500

    initial version of gzipper

---
+++ gzipper.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+ME=${0##*/}
+WORKDIR=$1
+GZIP=`which gzip`
+TARBALLS=`cd $WORKDIR 2>&1 && ls *.tar` 
+
+#case $1 in
+#(-\?|-h|-help|--help)
+#    usage
+#    exit 0 
+
+usage() {
+ echo " "
+ echo "$ME - For a given target directory, find *.tar and gzip it if"
+ echo "a .tar.gz does not already exist. Optionally create <foo>.tar.gz.md5"
+ echo " "
+ echo "Usage: $ME <target directory> [OPTIONS]"
+ echo "Example: $ME /mnt/somedir --md5"
+ echo "md5 default behavior if unset is not to create md5"
+ echo " "
+}
+
+if [ -z "$WORKDIR" ] ;
+ then
+  usage
+  exit 0 ;
+fi
+
+if [ ! -d "$WORKDIR" ]    
+ then
+  echo " "
+  echo "FATAL: Nonexistant target directory $WORKDIR!"
+  echo "Exiting..."
+  echo " "
+  exit 1
+fi
+
+if [ -z "$GZIP" ] ;
+ then
+  echo " "
+  echo "FATAL: no gzip in path!"
+  echo "Exiting..."
+  echo " "
+  exit 1 ;
+fi
+
+if [[ $2 == "--md5" ]]
+ then
+  MD5SUM=`which md5sum`
+   if [ -z $MD5SUM ] ;
+    then
+    echo " "
+    echo "FATAL: no md5sum in path!"
+    echo "Exiting..."
+    echo " "
+    exit 1 ;
+   fi
+fi
+
+for TAR in $TARBALLS ; do
+# if [ -f "$TAR.gz" ]; then
+#  echo " "
+#  echo "Ignoring existing $TAR.gz..."
+#  break
+# fi 
+ echo " "
+ echo "-------------------------------------------------------------------------"
+ echo "+ Processing $TAR -> $TAR.gz..."
+# $GZIP -1 $TAR 
+ $GZIP -1 -c $TAR > $TAR.gz 
+ if [[ $2 == "--md5" ]]
+  then
+   echo "| Creating $TAR.gz.md5..."
+   $MD5SUM $TAR.gz > $TAR.gz.md5
+   echo -n "|  " ; cat $TAR.gz.md5
+ fi
+ echo "-------------------------------------------------------------------------"
+done
+
+echo " "
+echo "Done!"
+echo " "
+exit 0
